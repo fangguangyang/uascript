b5d82ef16fbf2e50c1ea70fa40a60f9b92871eae
Julius Pfrommer <julius.pfrommer@web.de>
2015-09-25 12:14:23 +0200

    add the guid type

--- lua-open62541.c
+++ lua-open62541.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "lua5.2/lua.h"
 #include "lua5.2/lauxlib.h"
 #include "lua5.2/lualib.h"
@@ -98,12 +99,56 @@ static int nodeid_gc(lua_State *L) {
     return 0;
 }
 
+/* Guid */
+static int guid_new(lua_State *L) {
+    const char *arg;
+    if(lua_gettop(L) > 0) {
+        if(!lua_isstring(L, 1))
+            return luaL_error(L, "The argument needs to be a string of the form \"00000000-0000-0000-0000-000000000000\"");
+        size_t arglength;
+        arg = lua_tolstring(L, 1, &arglength);
+        if(arglength != 36)
+            return luaL_error(L, "The guid string has not the right format: \"00000000-0000-0000-0000-000000000000\"");
+    }
+    UA_Guid *id = lua_newuserdata(L, sizeof(UA_Guid));
+    UA_Guid_init(id);
+    luaL_getmetatable(L, "open62541-guid");
+    lua_setmetatable(L, -2);
+    if(lua_gettop(L) > 1) {
+        id->data1 = strtoull(arg, NULL, 16);
+        id->data2 = strtoull(&arg[9], NULL, 16);
+        id->data3 = strtoull(&arg[14], NULL, 16);
+        UA_Int16 data4_1 = strtoull(&arg[19], NULL, 16);
+        id->data4[0] = data4_1 >> 8;
+        id->data4[1] = data4_1;
+        UA_Int64 data4_2 = strtoull(&arg[24], NULL, 16);
+        id->data4[2] = data4_2 >> 40;
+        id->data4[3] = data4_2 >> 32;
+        id->data4[4] = data4_2 >> 24;
+        id->data4[5] = data4_2 >> 16;
+        id->data4[6] = data4_2 >> 8;
+        id->data4[7] = data4_2;
+    }
+    return 1;
+}
+
+static int guid_tostring(lua_State *L) {
+    UA_Guid *id = luaL_checkudata (L, -1, "open62541-guid");
+    char out[37];
+    int size = snprintf(out, 37, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+                        id->data1, id->data2, id->data3, id->data4[0], id->data4[1],
+                        id->data4[2], id->data4[3], id->data4[4], id->data4[5], id->data4[6], id->data4[7]);
+    lua_pushlstring(L, out, size);
+    return 1;
+}
+
 /***********************/
 /* Populate the Module */
 /***********************/
 
 static const struct luaL_Reg open62541 [] = {
     {"string", string_new},
+    {"guid", guid_new},
     {"nodeid", nodeid_new},
     {NULL, NULL} /* sentinel */
 };
@@ -115,6 +160,12 @@ static void register_builtintypes(lua_State *L) {
     lua_setfield(L, -2, "__tostring");
     lua_pushcfunction(L, string_gc);
     lua_setfield(L, -2, "__gc");
+    lua_pop(L, 1);
+
+    /* Guid */
+    luaL_newmetatable(L, "open62541-guid");
+    lua_pushcfunction(L, guid_tostring);
+    lua_setfield(L, -2, "__tostring");
     lua_pop(L, 1);
 
     /* NodeId */
